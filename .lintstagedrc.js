60c392f384381579abcfeec0433bacb156a57908
Usho Ka <kaushou@gmail.com>
2022-04-26 15:24:32 +0900

    Format JSON and markdown files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,4 @@
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{json,md}': 'prettier --write',
 };
